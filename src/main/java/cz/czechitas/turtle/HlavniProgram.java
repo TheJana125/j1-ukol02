bb60d1c0e40602ef997509da1ebb89b492af8b54
TheJana125 <jtheierova@gmail.com>
2022-04-23 17:33:09 +0200

    2.1 nakresli Zmrzlinu

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -11,82 +11,12 @@ public class HlavniProgram {
         zofka = new Turtle();
 
      nakresliZmrzlinu(zofka);
-     nakresliSnehulaka(zofka);
-     nakresliMasinku(zofka);
+
 
 
     }
 
-    private void nakresliMasinku(Turtle zofka) {
-        zofka.setLocation(1100.0, 500.0);
-        zofka.setPenWidth(3);
-        zofka.penDown();
-        nakresliObdelnik(zofka, 140.0, 100.0, Color.gray);
-        nakresliKolecko(zofka, 8, 40, Color.gray);
-        zofka.turnLeft(90.0);
-        zofka.move(100.0);
-        zofka.turnRight(90.0);
-        nakresliObdelnik(zofka, 80.0, 180.0, Color.gray);
-        zofka.penUp();
-        zofka.penUp();
-        zofka.turnLeft(180.0);
-        zofka.move(25.0);
-        zofka.turnRight(90.0);
-        zofka.move(40.0);
-        zofka.turnRight(90.0);
-        zofka.penDown();
-        nakresliKolecko(zofka, 5, 28, Color.gray);
-        zofka.penUp();
-        zofka.turnLeft(90.0);
-        zofka.move(70.0);
-        zofka.turnRight(90.0);
-        zofka.penDown();
-        nakresliKolecko(zofka, 5, 28, Color.gray);
-        zofka.penUp();
-        zofka.turnLeft(90.0);
-        zofka.move(150.0);
-        zofka.penDown();
-        zofka.turnRight(180.0);
-        nakresliRovnoramennyTrojuhelnik(zofka, 80.0,Color.gray);
-        zofka.penUp();
-    }
 
-    private void nakresliSnehulaka(Turtle zofka) {
-        zofka.setLocation(600.0,200.0);
-        zofka.turnLeft(90.0);
-        zofka.setPenWidth(3);
-        nakresliKolecko(zofka, 8, 30, Color.blue);
-        zofka.penUp();
-        zofka.turnLeft(180.0);
-        zofka.move(107.0);
-        zofka.turnLeft(90.0);
-        zofka.move(30.0);
-        zofka.turnLeft(90);
-        zofka.penDown();
-        nakresliKolecko(zofka,12, 36, Color.blue);
-        zofka.penUp();
-        zofka.turnLeft(180.0);
-        zofka.move(177.0);
-        zofka.turnLeft(90.0);
-        zofka.move(40.0);
-        zofka.turnLeft(90);
-        zofka.penDown();
-        nakresliKolecko(zofka,16, 42, Color.blue);
-        zofka.penUp();
-        zofka.move(182.0);
-        zofka.turnRight(90);
-        zofka.move(17.0);
-        zofka.turnLeft(90.0);
-        zofka.penDown();
-        nakresliKolecko(zofka,6, 30, Color.blue);
-        zofka.penUp();
-        zofka.turnLeft(90.0);
-        zofka.move(193.0);
-        zofka.turnRight(90.0);
-        zofka.penDown();
-        nakresliKolecko(zofka,6, 30, Color.blue);
-        zofka.penUp();
-    }
 
     private void nakresliZmrzlinu(Turtle zofka) {
         zofka.setLocation(150.0, 200.00);
